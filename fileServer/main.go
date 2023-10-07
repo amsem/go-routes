13696ff07e2d63fe3958c5e45763f36480d0e7d0
amsem <saidamine.selmani@fgei.ummto.dz>
2023-10-07 07:26:23 +0100

    add: somne changes

--- fileServer/main.go
+++ fileServer/main.go
@@ -9,6 +9,5 @@ import (
 
 func main() {
 	route := httprouter.New()
-	route.ServeFiles("/static/*filepath", http.Dir("/home/amsem/workspace/src/github.com/amsem/go-routes/fileServer/static"))
 	log.Fatal(http.ListenAndServe(":8000", route))
 }
